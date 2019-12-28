9d1fa726104bb6978caccd54fc727fd0a7bfca15
Anthony Quigel <quigela@gmail.com>
2019-12-27 23:15:33 -0500

    update to refresh console. added POV hat:

--- jstest2.c
+++ jstest2.c
@@ -1,26 +1,25 @@
 #include <windows.h>
 #include <stdio.h>
+
 int main()
 {
     int i;
     JOYINFOEX pji;
-    int update;
     DWORD axes[6] = {0};
     UINT buttons[13] = {0};
+    UINT hat[5] = {0};
     ZeroMemory(&pji, sizeof(pji));
     pji.dwSize = sizeof(pji);
     pji.dwFlags = JOY_RETURNBUTTONS | JOY_RETURNX | JOY_RETURNY
-        | JOY_RETURNZ | JOY_RETURNR | JOY_RETURNU | JOY_RETURNV;
+        | JOY_RETURNZ | JOY_RETURNR | JOY_RETURNU | JOY_RETURNV | JOY_RETURNPOV;
     while(JOYERR_NOERROR == joyGetPosEx(0, &pji))
     {
-        update = 0;
         for(i = 0; i < 6; i++)
         {
             DWORD axis = ((DWORD*) &(pji.dwXpos))[i];
             if(axis != axes[i])
             {
                 axes[i] = axis;
-                update = 1;
             }
         }
         for(i = 0; i < 13; i++)
@@ -29,20 +28,23 @@ int main()
             if(button != buttons[i])
             {
                 buttons[i] = button;
-                update = 1;
             }
         }
-        if(update)
-        {
-            printf("Axes:", pji.dwButtons);
+        //if(update)
+        //{
+            fprintf(stdout, "Axes:", pji.dwButtons);
             for(i = 0; i < 6; i++)
             {
-                printf("  %d: %5d", i, axes[i] + 32767);
+                fprintf(stdout, "  %d: %5d", i, axes[i] + 32767);
             }
-            printf(" Buttons:  ", pji.dwButtonNumber);
+            fprintf(stdout, "  Buttons:  ", pji.dwButtonNumber);
             for(i = 0; i < 13; i++)
-                printf("%d:%s  ", i, buttons[i] ? "on ":"off");
-            printf("\n");
-        }
+            {
+                fprintf(stdout, "%d:%s  ", i, buttons[i] ? "on ":"off");
+            }
+            printf("  Hat: %d", (pji.dwPOV/100));
+            fprintf(stdout, "\r");
+            fflush(stdout);
+        //}
     }
 }
